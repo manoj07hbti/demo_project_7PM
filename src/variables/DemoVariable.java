cc5ddc8a25ac8bab48ea4fa4191035115fd90588
60manojkumar@gmail.com <Motivated@2019>
2022-06-09 19:26:32 +0530

    Variables

---
+++ src/variables/DemoVariable.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class DemoVariable {
+
+    public static void main(String[] args) {
+
+//SYNTAX : Datatype var_name= value;
+
+        String city="AGRA";
+        String name="RAJ";
+        //RAJ
+
+        int age=23;
+        int height=6;
+        //23
+
+        //546.67
+        double price=546.67;
+        double percentage=45.78;
+    }
+}
