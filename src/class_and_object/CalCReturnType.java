13218a7c37ec9859ed645bd29bb50f66ceb4235e
60manojkumar@gmail.com <Motivated@2019>
2022-06-25 19:35:30 +0530

    Method Day 4

--- src/class_and_object/CalCReturnType.java
+++ src/class_and_object/CalCReturnType.java
@@ -10,11 +10,22 @@ public class CalCReturnType {
         return sum;
     }
 
+    public double divide(int a, int b){
+
+        double divide=(double) a/b;
+
+
+        return divide;
+    }
+
     public static void main(String[] args) {
         CalCReturnType obj = new CalCReturnType();
         int result = obj.add(3, 8);// store result
 
         System.out.println("Addition is " + result);
+
+       double divide = obj.divide(6,5);
+       System.out.println("Divide of a and b is "+divide);
 
     }
 }
